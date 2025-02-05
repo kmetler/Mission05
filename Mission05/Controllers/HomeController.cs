324039c54f681a2ab00bf4d98a776bd740112a0a
Kaitlyn <ksmetler@gmail.com>
2025-02-05 09:02:57 -0700

    Set up and finish index page

--- Mission05/Controllers/HomeController.cs
+++ Mission05/Controllers/HomeController.cs
@@ -18,15 +18,9 @@ namespace Mission05.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Calculator()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
