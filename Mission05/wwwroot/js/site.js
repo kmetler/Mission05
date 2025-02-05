1d9213a2aac8a9ee1de999596a089d98081725fe
Kaitlyn <ksmetler@gmail.com>
2025-02-05 10:05:00 -0700

    Finish calculator and styling

--- Mission05/wwwroot/js/site.js
+++ Mission05/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#calculate").click(function () {
+        let hours = $("#hours").val();
+        let rate = $("#rate").val();
+
+        if (hours <= 0 || isNaN(hours)) {
+            $("#error-message").show();
+            $("#total").val("");
+        } else {
+            $("#error-message").hide();
+            let total = hours * rate;
+            $("#total").val(total.toFixed(2));
+        }
+    });
+});
